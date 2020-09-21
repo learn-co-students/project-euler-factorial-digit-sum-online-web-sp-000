10e0f8c1e3d0d8f75f1bc26958d6bec07930e1fa
boom100100 <bsd231@nyu.edu>
2020-09-21 17:16:47 -0400

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,13 @@
 # code your solution here
+
+def factorial(n)
+  n > 1 ? n * (factorial(n - 1)) : n
+end
+
+def sum_of_digits(n)
+  n.digits.reduce(0, :+)
+end
+
+def factorial_digit_sum(n)
+  sum_of_digits(factorial(n))
+end
